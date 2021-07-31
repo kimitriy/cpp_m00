810f4df84b4dc8baf43f7095135af9b33299e5a4
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-07-31 23:03:54 +0300

    Add files via upload

---
+++ ex02/Account.cpp
@@ -0,0 +1,39 @@
+#include "Account.hpp"
+
+//public constructors
+Account::Account( int initial_deposit )
+{
+
+}
+
+//public destructors
+Account::~Account() {};
+
+//setters
+
+//getters
+int	Account::getNbAccounts( void )
+{
+	return (_accountIndex);
+}
+
+int	Account::getTotalAmount( void )
+{
+
+}
+
+int	Account::getNbDeposits( void )
+{
+
+}
+
+int	Account::getNbWithdrawals( void )
+{
+
+}
+
+//public m-methods
+void Account::displayStatus( void ) const
+{
+
+}
