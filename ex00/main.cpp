48aa9bcd5f2f89d24a71fe208452a271dc7b62d9
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-08-17 16:12:39 +0300

    Add files via upload

---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Raphael Burton on 7/14/21.
+//
+
+#include "megaphone.hpp"
+
+int		main(int argc, char **argv)
+{
+	std::string str;
+	int i;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (i < argc)
+		{
+			str = str + argv[i];
+			i++;
+		}
+		Megaphone blabla(str);
+		blabla.toUpperCase();
+	}
+	std::cout << std::endl;
+	return (0);
+}
